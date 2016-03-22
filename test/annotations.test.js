9636b588577749ef0546ca2c663d57e05da1009c
backhand <frederik@backhand.dk>
2016-03-22 14:19:45 +0100

    [test] Refactor to use mocha

--- test/annotations.test.js
+++ test/annotations.test.js
@@ -1,43 +1,39 @@
 var annotations = require('../lib/annotations');
+var assert = require('assert');
 
-function testfileTests(result, test) {
-  test.equal(result.testfunction1.annotation, 'annotation for testfunction1');
-  test.equal(result.testfunction1.someotherkey, 'someothervalue');
-  test.equal(result.testfunction2.annotation, 'annotation for testfunction2');
-  test.equal(result.anonymous.annotation, 'annotation for anonymous function export');
-  test.equal(result.anonymous.emptyannotation, true);
-  test.equal(result.anonymous.anotheremptyannotation, true);
-  test.deepEqual(result.testfunction3.duplicate, ['first','second','third']);
-  test.equal(result.testfunction4.hi, 'there');
-  test.equal(result.testfunction5.annotation, 'annotation for variable assigned function');
-  test.equal(result.testfunction6.annotation, 'test');
-  test.equal(result.testfunction7.annotation, 'test');
-  test.equal(result.testfunction8.annotation, 'test');
-  test.equal(result.testfunction9.annotation, 'test');
-  test.equal(result.testfunction10.annotation, 'test');
-  test.equal(result.testfunction11.annotation, 'test');
-  test.equal(result.testfunction12.annotation, 'test');
-  test.equal(result.testfunction13.annotation, 'test');
-  test.equal(result.testfunction14.annotation, 'test');
-  test.equal(result.testfunction15.annotation, 'test');
-  test.equal(result.testfunction16.annotation, 'test');
-}
-
-exports['Test 1: data/testfile.js, asynchronous'] = function(test) {
-  
-  annotations.get('test/data/testfile.js', function(err, result) {
-    
-    testfileTests(result, test);
-    
-    test.done();
+describe('annotations', function(done) {
+  it('should retrieve correct annotations from functions, async', function() {
+    annotations.get('test/data/testfile.js', function(err, result) {
+      testfileTests(result);
+      done();
+    });
   });
-};
 
-exports['Test 2: data/testfile.js, synchronous'] = function(test) {
-  
-  var result = annotations.getSync('test/data/testfile.js');
-  
-  testfileTests(result, test);
-  
-  test.done();
-};
+  it('should retrieve correct annotations from functions, sync', function() {
+    var result = annotations.getSync('test/data/testfile.js');
+    testfileTests(result);
+  });
+}); // End describe annotations
+
+function testfileTests(result) {
+  assert.equal(result.testfunction1.annotation, 'annotation for testfunction1');
+  assert.equal(result.testfunction1.someotherkey, 'someothervalue');
+  assert.equal(result.testfunction2.annotation, 'annotation for testfunction2');
+  assert.equal(result.anonymous.annotation, 'annotation for anonymous function export');
+  assert.equal(result.anonymous.emptyannotation, true);
+  assert.equal(result.anonymous.anotheremptyannotation, true);
+  assert.deepEqual(result.testfunction3.duplicate, ['first','second','third']);
+  assert.equal(result.testfunction4.hi, 'there');
+  assert.equal(result.testfunction5.annotation, 'annotation for variable assigned function');
+  assert.equal(result.testfunction6.annotation, 'test');
+  assert.equal(result.testfunction7.annotation, 'test');
+  assert.equal(result.testfunction8.annotation, 'test');
+  assert.equal(result.testfunction9.annotation, 'test');
+  assert.equal(result.testfunction10.annotation, 'test');
+  assert.equal(result.testfunction11.annotation, 'test');
+  assert.equal(result.testfunction12.annotation, 'test');
+  assert.equal(result.testfunction13.annotation, 'test');
+  assert.equal(result.testfunction14.annotation, 'test');
+  assert.equal(result.testfunction15.annotation, 'test');
+  assert.equal(result.testfunction16.annotation, 'test');
+}
